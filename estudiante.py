00392d2a94bf04fff84b86f38db2f9156042cd75
Tu Nombre <01johanalba@gmail.com>
2026-01-19 00:27:03 -0500

    Terminamos la clase estudiante y comenzamos con la clase sistema

--- estudiante.py
+++ estudiante.py
@@ -1,7 +1,7 @@
-import pandas as pd
+#import pandas as pd
 class Estudiante():
-    'clase estudiante'
-
+    
+    #clase estudiante
     def __init__(self,id_estudiante:str,nombre:str,edad:int):
         self.id_estudiante = id_estudiante
         self.nombre = nombre
@@ -9,9 +9,11 @@ class Estudiante():
         self.notas = {}
 
     def agregar_nota(self,materia, calificacion):    
-    # Agrega o actualiza la nota de una materia
-    # Validar que la calificación esté entre 0 y 10
-    # Retornar True si se agregó, False si es inválida
+    
+        # Agrega o actualiza la nota de una materia
+        # Validar que la calificación esté entre 0 y 10
+        # Retornar True si se agregó, False si es inválida
+       
         if calificacion >= 0 and calificacion <=10:
             self.notas [materia] = calificacion
             return True
@@ -22,16 +24,53 @@ class Estudiante():
         try:
             valores = self.notas.values()
             total = sum(valores)
-            self.promedio = total / len(valores)
-            return self.promedio
+            return  total / len(valores)
+ 
         except  ZeroDivisionError:
             return 0
 
-
-
-
-    def aprobado(self,nota_minima=6): #Verificar si aprobó
-        pass
+    def aprobado(self,nota_minima=6): 
+        
+        # Verifica si el estudiante aprobó
+        # Retorna True si promedio >= nota_minima, False en caso contrario
+        return self.promedio() >= nota_minima
     
-    def to_dict(): #Convertir a diccionario (para guardar en JSO
-        pass    
+    def to_dict(self): 
+        
+        # Convierte el estudiante a un diccionario
+        # Para poder guardarlo en JSON
+        # Retorna: {'id': ..., 'nombre': ..., 'edad': ..., 'notas': {...}
+        
+        return {
+            'id':self.id_estudiante,
+            'nombre':self.nombre,
+            'edad':self.edad,
+            'notas':self.notas
+        }
+    @staticmethod
+    def from_dict(data):
+        # Método estático (@staticmethod)
+        # Crea un objeto Estudiante desde un diccionario
+        # Útil para cargar datos desde JSON
+
+        id = data['id']
+        nombre = data['nombre']
+        edad = data['edad']
+        
+        estudiante = Estudiante(id,nombre,edad)
+        
+        estudiante.notas = data.get('notas',{})
+
+        return estudiante
+    
+    def __str__(self):
+        
+        # Representación legible del estudiante
+        # Ejemplo: "[001] Juan Pérez (20 años) - Promedio: 8.5 ✅ Aprobado"
+        promedio = self.promedio()
+        if self.aprobado():
+            estado = '✅ Aprobado'
+        else:
+            estado = '❌ Reprobado'
+        return f'[{self.id_estudiante}] {self.nombre} ({self.edad} años) - Promedio: {promedio:.2f} {estado}'
+        
